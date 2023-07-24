3fabefb6acb7ad9a21311bb74e5bfd2a486f48ed
yodavo <davidguevaraapaza@gmail.com>
2023-07-23 20:39:22 -0500

    update

--- 1-fetch-from-scientific-name.R
+++ 1-fetch-from-scientific-name.R
@@ -5,12 +5,18 @@ library(tidyverse)
 token_key = "get_your_own_LOL"
 
 #### 2. Function ####
-get.search <- function(species.list, token.key){
+get.search <- function(species.list, token.key, name){
   # a. Create the output dataframes
   df.iucn <- data.frame()
   count.iucn <- 0
   df.fails <- ""
   count.fails <- 0
+  
+  # remove NAs
+  species.list <- species.list[!is.na(species.list)]
+  
+  # remove duplicates and whitespaces
+  species.list <- trimws(unique(species.list))
   
   # loop
   for (specie in species.list){
@@ -27,61 +33,57 @@ get.search <- function(species.list, token.key){
   }
   writeLines(" ")
   writeLines(paste(count.iucn, " retrieved\n", count.fails, " fails.", sep = ""))
+  
+  # export iucn
+  write.csv(df.iucn, paste("output/iucn_", name, ".csv", sep = ""), 
+            row.names = FALSE)
+  # export review
+  write.csv(data.frame(df.fails), paste("output/review_", name, ".csv", sep = ""), 
+            row.names = FALSE)
+  
   return(list(df.iucn, df.fails))
 }
 
-#### 1. Load info from Megantoni ####
+#### Get the info ####
+
+#### Aves ####
 # 2 ebird checklist curated (with no duplications) from the Megantoni district in Peru
 megantoni_aves <- read_csv("input/aves-megantoni.csv", show_col_types = FALSE)$scientific_names
+# run the API
+iucn_aves <- get.search(megantoni_aves, token_key, "aves")
 
-# botanical data curated for previous works
-megantoni_plants <- read_csv("input/plants_megantoni.csv", show_col_types = FALSE)$species
-megantoni_plants <- megantoni_plants[!is.na(megantoni_plants)]
-
-# Herpetofauna data curated for previous works
-megantoni_herpe <- read_csv("input/herpetofauna_megantoni.csv", show_col_types = FALSE)$species
-megantoni_herpe <- megantoni_herpe[!is.na(megantoni_herpe)]
-
+#### Aves 2 ####
 # Review aves megantoni for previuos works
 megantoni_aves2 <- read_csv("input/rev-aves-megantoni.csv", show_col_types = FALSE)$species
-megantoni_aves2 <- megantoni_aves2[!is.na(megantoni_aves2)]
+# run the API
+iucn_aves2 <- get.search(megantoni_aves2, token_key, "aves2")
 
+#### Plants ####
+# botanical data curated for previous works
+megantoni_plants <- read_csv("input/plants_megantoni.csv", show_col_types = FALSE)$species
+# run the API
+iucn_plants <- get.search(megantoni_plants, token_key, "plantas")
+
+#### Herpetofauna ####
+# Herpetofauna data curated for previous works
+megantoni_herpe <- read_csv("input/herpetofauna_megantoni.csv", show_col_types = FALSE)$species
+# Run the API
+iucn_herpe <- get.search(megantoni_herpe, token_key, "herpe")
+
+#### Mammals ####
 # Review mammals megantoni for previuos works
 megantoni_mamm <- read_csv("input/mammals_megantoni.csv", show_col_types = FALSE)$species
-megantoni_mamm <- megantoni_aves2[!is.na(megantoni_mamm)]
+# run the API
+iucn_mamm <- get.search(megantoni_mamm, token_key, "mammals")
 
-#### 2. Run API ####
-# Aves
-iucn_aves <- get.search(megantoni_aves, token_key)
-# export csv
-write.csv(iucn_aves[[1]], "output/iucn_aves.csv", row.names = FALSE)
-# export review
-write.csv(data.frame(iucn_aves[[2]]), "output/review_aves.csv", row.names = FALSE)
+#### B10K ####
+# Review mammals megantoni for previuos works
+b10k <- read_csv("input/B10K_dont_collect.csv", show_col_types = FALSE)$species
+# run the API
+iucn_b10k <- get.search(b10k, token_key, "b10k")
 
-# Plant
-iucn_plants <- get.search(megantoni_plants, token_key)
-# export csv
-write.csv(iucn_plants[[1]], "output/iucn_plants.csv", row.names = FALSE)
-# export review
-write.csv(data.frame(iucn_plants[[2]]), "output/review_plants.csv", row.names = FALSE)
-
-# Herpetofauna
-iucn_herpe <- get.search(megantoni_herpe, token_key)
-# export csv
-write.csv(iucn_herpe[[1]], "output/iucn_herpe.csv", row.names = FALSE)
-# export review
-write.csv(data.frame(iucn_herpe[[2]]), "output/review_herpe.csv", row.names = FALSE)
-
-# Aves 2
-iucn_aves2 <- get.search(megantoni_aves2, token_key)
-# export csv
-write.csv(iucn_aves2[[1]], "output/iucn_aves2.csv", row.names = FALSE)
-# export review
-write.csv(data.frame(iucn_aves2[[2]]), "output/review_aves2.csv", row.names = FALSE)
-
-# mammals
-iucn_mamm <- get.search(megantoni_mamm, token_key)
-# export csv
-write.csv(iucn_mamm[[1]], "output/iucn_mammals.csv", row.names = FALSE)
-# export review
-write.csv(data.frame(iucn_mamm[[2]]), "output/review_mammals.csv", row.names = FALSE)
+#### Aves - rerun ####
+# Review mammals megantoni for previuos works
+megantoni_aves3 <- read_csv("input/reviewed_aves.csv", show_col_types = FALSE)$new_name
+# run the API
+iucn_aves3 <- get.search(megantoni_aves3, token_key, "aves3")
